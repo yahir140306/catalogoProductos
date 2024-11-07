c18519826a410ed9c2ea0bd7f8e138f7bed1be90
yahir140306 <joseyahirhernandez11@gmail.com>
2024-11-07 01:33:49 -0600

    first commit

---
+++ src/components/Products/Productos.js
@@ -0,0 +1,50 @@
+export const PRODUCTOS = [
+    {
+        foto: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.arteli.com.mx%2Fjuguete-para-bebes-fisher-price-perrito-3506713%2Fp&psig=AOvVaw1lEL4AeB0W4FBgLnITS3se&ust=1731050643451000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPiV5KfYyYkDFQAAAAAdAAAAABAE",
+        nombre: "Producto 1 - Nombre",
+        descripcion: "Producto 1",
+        caracteristicas: "Caracteristicas del producto 1",
+        dimensiones: "Dimensiones del producto 1",
+        material: "Material del producto 1",
+        instrucciones: "Instrucciones del producto 1",
+        precauciones: "Precauciones del producto 1",
+        precio: 100,
+        descuento: 10
+    },
+    {
+        foto: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.arteli.com.mx%2Fjuguete-para-bebes-fisher-price-perrito-3506713%2Fp&psig=AOvVaw1lEL4AeB0W4FBgLnITS3se&ust=1731050643451000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPiV5KfYyYkDFQAAAAAdAAAAABAE",
+        nombre: "Producto 1  - Nombre",
+        descripcion: "Producto 1",
+        caracteristicas: "Caracteristicas del producto 1",
+        dimensiones: "Dimensiones del producto 1",
+        material: "Material del producto 1",
+        instrucciones: "Instrucciones del producto 1",
+        precauciones: "Precauciones del producto 1",
+        precio: 100,
+        descuento: 10
+    },
+    {
+        foto: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.arteli.com.mx%2Fjuguete-para-bebes-fisher-price-perrito-3506713%2Fp&psig=AOvVaw1lEL4AeB0W4FBgLnITS3se&ust=1731050643451000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPiV5KfYyYkDFQAAAAAdAAAAABAE",
+        nombre: "Producto 1",
+        descripcion: "Producto 1",
+        caracteristicas: "Caracteristicas del producto 1",
+        dimensiones: "Dimensiones del producto 1",
+        material: "Material del producto 1",
+        instrucciones: "Instrucciones del producto 1",
+        precauciones: "Precauciones del producto 1",
+        precio: 100,
+        descuento: 10
+    },
+    {
+        foto: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.arteli.com.mx%2Fjuguete-para-bebes-fisher-price-perrito-3506713%2Fp&psig=AOvVaw1lEL4AeB0W4FBgLnITS3se&ust=1731050643451000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPiV5KfYyYkDFQAAAAAdAAAAABAE",
+        nombre: "Producto 1",
+        descripcion: "Producto 1",
+        caracteristicas: "Caracteristicas del producto 1",
+        dimensiones: "Dimensiones del producto 1",
+        material: "Material del producto 1",
+        instrucciones: "Instrucciones del producto 1",
+        precauciones: "Precauciones del producto 1",
+        precio: 100,
+        descuento: 10
+    },
+]
